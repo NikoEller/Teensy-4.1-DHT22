c4e11e57992af72aa15c6cdf22fe70d5308501bd
NikoEller <niko.eller@live.com>
2023-07-25 17:41:15 +0200

    added SD card setup

--- DHT22/DHT22/DHT22.ino
+++ DHT22/DHT22/DHT22.ino
@@ -1,17 +1,40 @@
 #include "DHT.h" //DHT Bibliothek laden
+#include <SD.h>
+#include <SPI.h>
 
 #define DHTPIN 14 //Der Sensor wird an PIN 14 angeschlossen    
 
 #define DHTTYPE DHT22    // Es handelt sich um den DHT22 Sensor
 
 DHT dht(DHTPIN, DHTTYPE); //Der Sensor wird ab jetzt mit „dth“ angesprochen
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+File myFile;
+const int chipSelect = BUILTIN_SDCARD;
+const int buildInLEDPin = 13;
+
+
+const String fileName = "data.txt";
+
+
 
 
 
 void setup() {
   Serial.begin(9600); //Serielle Verbindung starten
-
+  sdCardSetup();
   dht.begin(); //DHT11 Sensor starten
+}
+
+void sdCardSetup(){
+    Serial.println("Initializing SD card...");
+
+  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
 }
 
 void loop() {
@@ -25,8 +48,5 @@ void loop() {
   
   Serial.println(Temperatur);
   Serial.println(Luftfeuchtigkeit); //die Dazugehörigen Werte anzeigen
-
-
-
 
 }
