021551c57d89c82291ab5b045d31111e781aa1ff
NikoEller <niko.eller@live.com>
2023-07-25 17:32:27 +0200

    Setup

--- DHT22/DHT22/DHT22.ino
+++ DHT22/DHT22/DHT22.ino
@@ -1,6 +1,17 @@
+#include "DHT.h" //DHT Bibliothek laden
+
+#define DHTPIN 14 //Der Sensor wird an PIN 14 angeschlossen    
+
+#define DHTTYPE DHT22    // Es handelt sich um den DHT22 Sensor
+
+DHT dht(DHTPIN, DHTTYPE); //Der Sensor wird ab jetzt mit „dth“ angesprochen
+
+
 
 void setup() {
+  Serial.begin(9600); //Serielle Verbindung starten
 
+  dht.begin(); //DHT11 Sensor starten
 }
 
 void loop() {
